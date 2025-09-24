233f44e55f4b953b96a24a808bac5ad7363df4d8
petafarad <210076523+petafarad@users.noreply.github.com>
2025-09-24 09:45:48 +0200

    calculator

---
+++ calculator.cs
@@ -0,0 +1,12 @@
+﻿float num1= 0;
+float num2= 0;
+Console.WriteLine("Enter first number:");
+while (!float.TryParse(Console.ReadLine(), out num1))
+    {
+    Console.WriteLine("Invalid input. Please enter a valid integer for the first number:");
+}
+Console.WriteLine("Enter second number:");
+while (!float.TryParse(Console.ReadLine(), out num2))
+{
+    Console.WriteLine("Invalid input. Please enter a valid integer for the second number:");
+}
